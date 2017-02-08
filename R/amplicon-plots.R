39baed3ea93a532149e23e309d6453c63e96c11a
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2017-02-08 10:54:41 -0500

    Update amplicon plots

--- R/amplicon-plots.R
+++ R/amplicon-plots.R
@@ -8,15 +8,15 @@ NULL
 #' from paired reads, a \code{graphNEL} object encapsulates this data.
 #' Plotting utilities in the \code{Rgraphviz} package can be used to
 #' visualize these graphs.
-#' 
+#'
 #' @export
-#' 
+#'
 #' @seealso See \code{\link[svcnvs]{sv_amplicon_exp}} for constructing
 #'   an \code{AmpliconGraph} object. See
 #'   \code{\link{qualitativeColors}} for a list of qualitative
 #'   colors. See \code{\link[graph]{graphNEL-class}} for details on
 #'   graph representation.
-#' 
+#'
 #' @param ag a \code{AmpliconGraph} object
 #' @param col_list a list of color palettes
 plot_amplicons <- function(ag, col_list=qualitativeColors()){
@@ -25,11 +25,16 @@ plot_amplicons <- function(ag, col_list=qualitativeColors()){
     return(invisible())
   }
   dark_colors <- c("#332288", "#661100", "#882255", "#AA4499")
-  ar <- ampliconRanges(ag)
-  ar <- keepSeqlevels(ar, unique(chromosome(ar)))
-  L <- length(seqlevels(ar))
+  ##ar <- ampliconRanges(ag)
+  ar <- nodes(ag)
+  ##ar <- keepSeqlevels(ar, unique(chromosome(ar)))
+  chroms <- sapply(strsplit(ar, ":"), "[", 1)
+  sl <- unique(chroms)
+  ##sl <- seqlevels(ar)
+  L <- length(sl)
   L <- ifelse(L > 12, 12, L)
-  sl <- factor(chromosome(ar), levels=seqlevels(ar))
+  ##sl <- factor(chromosome(ar), levels=sl)
+  sl <- factor(chroms, levels=sl)
   color_nodes <- col_list[[L]][as.integer(sl)]
   g1 <- graph(ag)
   color_nodes <- setNames(color_nodes, nodes(g1))
